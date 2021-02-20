54e1c175632398394ccadfc60a17c1f3971906dc
43032_Dipak <dipak.maske7@gmail.com>
2021-02-20 21:58:30 +0530

    second commit

---
+++ PMTool/src/main/java/com/app/dao/ProjectRepo.java
@@ -0,0 +1,15 @@
+package com.app.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.pojos.Project;
+
+@Repository
+public interface ProjectRepo extends CrudRepository<Project, Integer>{
+
+	Project findByProjectIdentifier(String projectId);
+	
+	@Override
+	Iterable<Project>findAll();
+}
